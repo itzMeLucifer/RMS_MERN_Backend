a0df52f1e277ea0ead66986ab72de2a8d22e6b6b
itzMeLucifer <devsab1997@gmail.com>
2023-04-18 13:22:02 +0530

    main

--- index.js
+++ index.js
@@ -25,11 +25,6 @@ app.use(helmet());
 app.use(morgan('common'));
 app.use(fileupload({ useTempFiles:true}))
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.use('/api',authRoutes)
 app.use('/api/products',productRoutes)
