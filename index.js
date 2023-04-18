92676e1b9bbb8a63c04099fbcefc970ecd8583e5
itzMeLucifer <devsab1997@gmail.com>
2023-04-18 13:10:20 +0530

    main

--- index.js
+++ index.js
@@ -19,14 +19,17 @@ const app = express();
 
 app.use(bodyParser.json({limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(helmet());
 app.use(morgan('common'));
 app.use(fileupload({ useTempFiles:true}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/api',authRoutes)
 app.use('/api/products',productRoutes)
