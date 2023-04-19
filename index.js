66a29d58a91508209de5285760539c44c1c9337d
itzMeLucifer <devsab1997@gmail.com>
2023-04-19 22:05:12 +0530

    man

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ const app = express();
 app.use(bodyParser.json({limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
 app.use(cors(
-    {
-        allowedHeaders:"*",
-        allowMethods:"*",
-        origin:"*"
-    }
 ));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
 
 app.use(cookieParser());
 app.use(helmet());
